98914ce5c4457d56525e0e6b47522c16d489595d
victory-na <409896753@qq.com>
2020-09-14 17:47:10 +0800

    Site updated: 2020-09-14 17:47:09

--- js/utils.js
+++ js/utils.js
@@ -1,86 +1,149 @@
-// eslint-disable-next-line no-unused-vars
-function waitElementVisible(targetId, callback) {
-  var runningOnBrowser = typeof window !== 'undefined';
-  var isBot = (runningOnBrowser && !('onscroll' in window)) || (typeof navigator !== 'undefined'
-    && /(gle|ing|ro|msn)bot|crawl|spider|yand|duckgo/i.test(navigator.userAgent));
-  var supportsIntersectionObserver = runningOnBrowser && 'IntersectionObserver' in window;
-  if (!isBot && supportsIntersectionObserver) {
-    var io = new IntersectionObserver(function(entries, ob) {
-      if (entries[0].isIntersecting) {
-        callback && callback();
-        ob.disconnect();
+/* eslint-disable no-unused-vars */
+
+var btf = {
+  debounce: function (func, wait, immediate) {
+    let timeout
+    return function () {
+      const context = this
+      const args = arguments
+      const later = function () {
+        timeout = null
+        if (!immediate) func.apply(context, args)
       }
-    }, {
-      threshold : [0],
-      rootMargin: (window.innerHeight || document.documentElement.clientHeight) + 'px'
-    });
-    io.observe(document.getElementById(targetId));
-  } else {
-    callback && callback();
-  }
-}
+      const callNow = immediate && !timeout
+      clearTimeout(timeout)
+      timeout = setTimeout(later, wait)
+      if (callNow) func.apply(context, args)
+    }
+  },
 
-// eslint-disable-next-line no-unused-vars
-function waitElementLoaded(targetId, callback) {
-  var runningOnBrowser = typeof window !== 'undefined';
-  var isBot = (runningOnBrowser && !('onscroll' in window)) || (typeof navigator !== 'undefined'
-    && /(gle|ing|ro|msn)bot|crawl|spider|yand|duckgo/i.test(navigator.userAgent));
-  if (!runningOnBrowser || isBot) {
-    return;
-  }
+  throttle: function (func, wait, options) {
+    let timeout, context, args
+    let previous = 0
+    if (!options) options = {}
 
-  if ('MutationObserver' in window) {
-    var mo = new MutationObserver(function(records, ob) {
-      var ele = document.getElementById(targetId);
-      if (ele) {
-        callback && callback();
-        ob.disconnect();
-      }
-    });
-    mo.observe(document, { childList: true, subtree: true });
-  } else {
-    var oldLoad = window.onload;
-    window.onload = function() {
-      oldLoad && oldLoad();
-      callback && callback();
-    };
-  }
-}
+    const later = function () {
+      previous = options.leading === false ? 0 : new Date().getTime()
+      timeout = null
+      func.apply(context, args)
+      if (!timeout) context = args = null
+    }
 
-// eslint-disable-next-line no-unused-vars
-function addScript(url, onload) {
-  var s = document.createElement('script');
-  s.setAttribute('src', url);
-  s.setAttribute('type', 'text/javascript');
-  s.setAttribute('charset', 'UTF-8');
-  s.async = false;
-  if (typeof onload === 'function') {
-    if (window.attachEvent) {
-      s.onreadystatechange = function() {
-        var e = s.readyState;
-        if (e === 'loaded' || e === 'complete') {
-          s.onreadystatechange = null;
-          onload();
+    const throttled = function () {
+      const now = new Date().getTime()
+      if (!previous && options.leading === false) previous = now
+      const remaining = wait - (now - previous)
+      context = this
+      args = arguments
+      if (remaining <= 0 || remaining > wait) {
+        if (timeout) {
+          clearTimeout(timeout)
+          timeout = null
         }
-      };
+        previous = now
+        func.apply(context, args)
+        if (!timeout) context = args = null
+      } else if (!timeout && options.trailing !== false) {
+        timeout = setTimeout(later, remaining)
+      }
+    }
+
+    return throttled
+  },
+
+  sidebarPaddingR: () => {
+    const innerWidth = window.innerWidth
+    const clientWidth = document.body.clientWidth
+    const paddingRight = innerWidth - clientWidth
+    if (innerWidth !== clientWidth) {
+      $('body').css('padding-right', paddingRight)
+    }
+  },
+
+  scrollToDest: name => {
+    const scrollOffset = $(name).offset().top
+    let offset
+    if ($(window).scrollTop() > scrollOffset) {
+      offset = 65
     } else {
-      s.onload = onload;
+      offset = 0
+    }
+    $('body,html').animate({
+      scrollTop: scrollOffset - offset
+    })
+  },
+
+  snackbarShow: (text, showAction, duration) => {
+    const sa = (typeof showAction !== 'undefined') ? showAction : false
+    const dur = (typeof duration !== 'undefined') ? duration : 2000
+    const position = GLOBAL_CONFIG.Snackbar.position
+    const bg = document.documentElement.getAttribute('data-theme') === 'light' ? GLOBAL_CONFIG.Snackbar.bgLight : GLOBAL_CONFIG.Snackbar.bgDark
+    Snackbar.show({
+      text: text,
+      backgroundColor: bg,
+      showAction: sa,
+      duration: dur,
+      pos: position
+    })
+  },
+
+  initJustifiedGallery: function (selector) {
+    selector.each(function (i, o) {
+      if ($(this).is(':visible')) {
+        $(this).justifiedGallery({
+          rowHeight: 220,
+          margins: 4
+        })
+      }
+    })
+  },
+
+  diffDate: (d, more = false) => {
+    const dateNow = new Date()
+    const datePost = new Date(d)
+    const dateDiff = dateNow.getTime() - datePost.getTime()
+    const minute = 1000 * 60
+    const hour = minute * 60
+    const day = hour * 24
+    const month = day * 30
+
+    let result
+    if (more) {
+      const monthCount = dateDiff / month
+      const dayCount = dateDiff / day
+      const hourCount = dateDiff / hour
+      const minuteCount = dateDiff / minute
+
+      if (monthCount > 12) {
+        result = datePost.toLocaleDateString().replace(/\//g, '-')
+      } else if (monthCount >= 1) {
+        result = parseInt(monthCount) + ' ' + GLOBAL_CONFIG.date_suffix.month
+      } else if (dayCount >= 1) {
+        result = parseInt(dayCount) + ' ' + GLOBAL_CONFIG.date_suffix.day
+      } else if (hourCount >= 1) {
+        result = parseInt(hourCount) + ' ' + GLOBAL_CONFIG.date_suffix.hour
+      } else if (minuteCount >= 1) {
+        result = parseInt(minuteCount) + ' ' + GLOBAL_CONFIG.date_suffix.min
+      } else {
+        result = GLOBAL_CONFIG.date_suffix.just
+      }
+    } else {
+      result = parseInt(dateDiff / day)
+    }
+    return result
+  },
+
+  loadComment: (dom, callback) => {
+    if ('IntersectionObserver' in window) {
+      const observerItem = new IntersectionObserver((entries) => {
+        if (entries[0].isIntersecting) {
+          callback()
+          observerItem.disconnect()
+        }
+      }, { threshold: [0] })
+      observerItem.observe(dom)
+    } else {
+      callback()
     }
   }
-  var e = document.getElementsByTagName('script')[0]
-    || document.getElementsByTagName('head')[0]
-    || document.head || document.documentElement;
-  e.parentNode.insertBefore(s, e);
-}
-
-// eslint-disable-next-line no-unused-vars
-function addCssLink(url) {
-  var l = document.createElement('link');
-  l.setAttribute('rel', 'stylesheet');
-  l.setAttribute('type', 'text/css');
-  l.setAttribute('href', url);
-  var e = document.getElementsByTagName('link')[0]
-    || document.getElementsByTagName('head')[0]
-    || document.head || document.documentElement;
-  e.parentNode.insertBefore(l, e);
 }
